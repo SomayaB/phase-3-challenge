95234cb0a6c13d077d362fc6db2ea65d8de4a5d8
SomayaB <somaya.bounouar@gmail.com>
2017-08-07 10:33:43 -0700

    Implement GET route

--- part-1/app.js
+++ part-1/app.js
@@ -6,7 +6,28 @@ var port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.get('/api/days/:day', function(request, response, next){
+  var daysOfWeek = {
+    monday: 1,
+    tuesday:2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6,
+    sunday: 7
+  }
+  var day = request.params.day.toLowerCase()
 
+  response.set('Content-Type', 'application/text')
+
+  if(!daysOfWeek.hasOwnProperty(day)) {
+    response.status(400)
+    response.send(`'${day}' is not a valid day!`)
+  } else {
+    response.status(200)
+    response.send(String(daysOfWeek[day]))
+  }
+})
 
 
 app.listen(port, function(){
