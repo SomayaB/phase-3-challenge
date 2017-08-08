78e02c38ffd3649a17fa73abc1b8d597003fe490
SomayaB <somaya.bounouar@gmail.com>
2017-08-08 11:34:10 -0700

    Add total sum to cart modal

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -6,29 +6,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var cartModal = document.querySelector('.cart-modal')
   var closeModalBtn = document.querySelector('.close-modal')
   var shoppingList = document.querySelector('.shopping-list')
+  var cartTotalNumber = document.querySelector('.cart-total-number')
 
-  ;(function cartCount(){
+  ;(function addToCart(){
       var itemsInCart = []
       var cartCount = 0
+      var cartTotal = 0.00
       addItemButtons.forEach(function(button){
         button.addEventListener('click', function(){
           var itemName = button.parentNode.querySelector('.item-name').textContent
           var itemPrice = button.parentNode.querySelector('.item-price').textContent
           itemsInCart.push(`${itemName} ${itemPrice}`)
-          console.log(itemsInCart);
           shoppingList.appendChild(document.createElement('p')).innerHTML=`${itemName} <span class="added-item-price">${itemPrice}</span>`
 
-
-
+          //seperate this out? updateCartCount()
           cartCount = itemsInCart.length
           cartCountDisplay.textContent = `(${cartCount})`
+
+          //seperate this out? updateCartTotal()
+          var priceWithoutDollarSign = Number(itemPrice.replace(/[^0-9\.]+/g,""))
+          cartTotal += priceWithoutDollarSign
+          var total = Math.round(cartTotal * 100) / 100
+          cartTotalNumber.textContent = total
         })
       })
   })()
-
-  // ;(function addToCart(){
-  //
-  // })
 
 
   ;(function openModal(){
