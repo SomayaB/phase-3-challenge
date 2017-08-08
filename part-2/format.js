42bf8986fb2f661b1402a5de9567e687e1e35e2c
SomayaB <somaya.bounouar@gmail.com>
2017-08-08 15:55:39 -0700

    Refactor formatting

--- part-2/format.js
+++ part-2/format.js
@@ -1,5 +1,24 @@
 
 
+function formatProductList(result){
+  console.log(`|------------------+------------------+`)
+  console.log(`|   Product Name   |      Section     |`)
+  console.log(`|------------------+------------------+`)
+  format(result)
+}
+function formatShopperOrders(result){
+  console.log(`|------------------+------------------+`)
+  console.log(`|     order id     |     total cost   |`)
+  console.log(`|------------------+------------------+`)
+  format(result)
+}
+function formatRealShoppers(result){
+  console.log(`|------------------+------------------+`)
+  console.log(`|   shopper name   | number of orders |`)
+  console.log(`|------------------+------------------+`)
+  format(result)
+}
+
 function format(result){
   var currentLine = result.forEach(function(line){
     var firstWord = String(Object.values(line)[0])
@@ -11,4 +30,8 @@ function format(result){
   console.log(`|------------------+------------------+`)
 }
 
-module.exports = format
+module.exports = {
+  formatProductList,
+  formatShopperOrders,
+  formatRealShoppers
+}
