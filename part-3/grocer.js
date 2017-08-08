a31714bca05c8e7006ddf5488ef8ebc4f3a0efaf
SomayaB <somaya.bounouar@gmail.com>
2017-08-07 22:04:27 -0700

    Add modal

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,22 +1,33 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-var cartCountDisplay = document.querySelector("#cart-item-count");
-var addItemButtons = document.querySelectorAll(".add-item-btn")
+  var cartCountDisplay = document.querySelector("#cart-item-count");
+  var addItemButtons = document.querySelectorAll(".add-item-btn")
+  var cartButton = document.querySelector("#cart-button")
+  var cartModal = document.querySelector('.cartModal')
 
-var itemsInCart = []
+  ;(function cartCount(){
+      var itemsInCart = []
+      var cartCount = 0
+      addItemButtons.forEach(function(button){
+        button.addEventListener('click', function(){
+          var itemName = button.parentNode.querySelector('.item-name').textContent
+          var itemPrice = button.parentNode.querySelector('.item-price').textContent
+          itemsInCart.push(`${itemName} ${itemPrice}`)
+          console.log(itemsInCart);
+          cartCount = itemsInCart.length
+          cartCountDisplay.textContent = `(${cartCount})`
+        })
+      })
+  })()
 
-  function cartCount(){
-    var cartCount = 0
-    addItemButtons.forEach(function(button){
-      button.addEventListener('click', function(){
-        var itemName = button.parentNode.querySelector('.item-name').textContent
-        var itemPrice = button.parentNode.querySelector('.item-price').textContent
-        itemsInCart.push(`${itemName} ${itemPrice}`)
-        console.log(itemsInCart);
-        cartCount = itemsInCart.length
-        cartCountDisplay.textContent = cartCount
+  ;(function openModal(){
+    cartButton.addEventListener('click', function(){
+      cartButton.addEventListener("click", function(){
+        cartModal.style.display = "block"
       })
     })
-  }
-cartCount()
+  })()
+
+
+
 })
